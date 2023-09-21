e0a179337673b2b35da6ae1e6a900d23095ff4b5
ProfessorAndersonChoren <anderson.choren@faqi.edu.br>
2023-09-21 19:17:00 -0300

    Calculadora JS

---
+++ Calculadora JS/conceitos_basicos.js
@@ -0,0 +1,9 @@
+// Saida de dados
+// 1° Forma
+alert("Hello World!!!");
+
+// 2° Forma
+console.log("No worry, this a simple message!!!");
+console.info("This is the message of information");
+console.warn("Hey, this a important message!!!");
+console.error("Ops, a system have burn, run my friend!!!");
